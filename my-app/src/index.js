7cfff4e1404a56f8243dc2f8fdd042be7920a91c
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-05-21 19:44:04 +0530

    handle click

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -25,6 +25,11 @@ class Board extends React.Component {
       squares: Array(9).fill(null),
     };
   }
+    handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
   renderSquare(i) {
     return (
       <Square value={this.state.squares[i]}
@@ -32,6 +37,8 @@ class Board extends React.Component {
       />
     );
   }
+
+
 
   render() {
     const status = 'Next player: X';
